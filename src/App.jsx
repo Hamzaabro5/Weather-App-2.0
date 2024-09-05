b99c24761b357a5fd9c91a3250295e96e41788d0
Hamzaabro5 <hamzaabro683@gmail.com>
2024-09-05 23:45:29 +0500

    React Weather App

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,57 @@
-import React, { useRef, useState } from 'react'
+import React, { useRef, useState , useEffect } from 'react'
 import axios from 'axios'
-
+import Card from './components/Card'
 
 function App() {
-  const [weather , setWeather] = useState()
+  const [weather , setWeather] = useState([])
   const inputVal = useRef()
 
 
-    function getCity(event) {
+    async function getCity(event) {
       event.preventDefault();
-      axios(`https://api.weatherapi.com/v1/current.json?key=982f5c58a9e742e68e5103048241306&q=${inputVal.current.value}&aqi=n`)
-      .then((res) => {  
-        console.log(res.data)
-        setWeather(res.data)
-        setWeather(res.data)
-      }).catch((err) => {
-        console.log(err);
-        alert(`No City found Please try Another!`)
-      })
+      try {
+
+      let weatherData = await axios(`https://api.weatherapi.com/v1/current.json?key=982f5c58a9e742e68e5103048241306&q=${inputVal.current.value}&aqi=n`)
+      console.log(weatherData.data);
+      weather.push(weatherData)
+      setWeather([...weather])
+      console.log(weather);
+      
+      } catch (error) {
+        alert(`Enter a Valid City Name!`)
+      }
+      
+      
       inputVal.current.value = ``
     }
+    
 
   return (
     <>
     <div>
-      <h1>Weathgit remote add origin https://github.com/Hamzaabro5/Weather-App-2.0.giter App</h1>
+      <h1>Weather App</h1>
       <form>
         <input type="text" ref={inputVal}/>
         <button onClick={getCity} type="submit">Search</button>
       </form>
+
+       <div className='flex justify-center gap-2 flex-wrap mt-3'>
+                {weather.length > 0 && (weather.map((item,index) => (
+                    <div key={index}>
+                        <Card location={item.data.location} condition={item.data.current}/>
+                    </div>
+                )))}
+            </div>
+      
+
+
+
     </div>
     </>
   )
 }
 
 export default App
+
+
+
